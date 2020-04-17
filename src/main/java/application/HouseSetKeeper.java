2367c89d31a0b09d322839bc1425e8962c9394e2
KoBold <kovacsboldizsar@gmail.com>
2020-04-17 16:37:45 +0300

    Houses loaded from file on separate thread

--- src/main/java/application/HouseSetKeeper.java
+++ src/main/java/application/HouseSetKeeper.java
@@ -1,11 +1,10 @@
 package application;
 
 import application.model.House;
+import application.utils.HouseScraper;
 import lombok.Getter;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
@@ -17,13 +16,33 @@ public class HouseSetKeeper {
 
     private final FileWriter fw;
 
-    public HouseSetKeeper() throws IOException {
+    public HouseSetKeeper() throws Exception {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream resourceAsStream = classLoader.getResourceAsStream("config.properties");
 
         Properties prop = new Properties();
         prop.load(resourceAsStream);
-        fw = new FileWriter(prop.getProperty("saved-urls"), true);
+        String savedUrlFile = prop.getProperty("saved-urls");
+
+        this.fw = new FileWriter(savedUrlFile, true);
+        loadHousesFromFile(savedUrlFile);
+    }
+
+    private void loadHousesFromFile(String file) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        new Thread(() -> {
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    House h = HouseScraper.scrapeHouse(line);
+                    synchronized (houseSet) {
+                        houseSet.add(h);
+                    }
+                }
+            } catch (Exception ignore) {
+            }
+        }).start();
     }
 
     public void persistHouse(House h) throws IOException {
--- src/main/java/application/utils/HouseScraper.java
+++ src/main/java/application/utils/HouseScraper.java
@@ -54,6 +54,4 @@ public class HouseScraper {
             return null;
         }
     }
-
-
 }
