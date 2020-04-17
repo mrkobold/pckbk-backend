b2ed0a42d550b0458aceb2953b552098eceb84d8
KoBold <kovacsboldizsar@gmail.com>
2020-04-17 14:03:19 +0300

    Refactor Request costruction

--- src/main/java/framework/Request.java
+++ src/main/java/framework/Request.java
@@ -1,46 +1,73 @@
 package framework;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
+@Slf4j
 public class Request {
     private final HttpMethod method;
     private final String uri;
     private final String protocol;
+    private final Map<String, String> headers = new HashMap<>();
     private final Map<String, String> parameters = new HashMap<>();
 
-    Request(String requestWholeText) {
-        String[] requestHeader = requestWholeText.split("\n")[0].split(" ");
-        this.method = HttpMethod.valueOf(requestHeader[0]);
-        this.protocol = requestHeader[2];
+    Request(String requestWholeText) throws Exception {
+
+        String[] firstLine = null;
+        int i;
+        for (i = 1; i < requestWholeText.length(); i++) {
+            if (requestWholeText.charAt(i - 1) == '\r' && requestWholeText.charAt(i) == '\n') {
+                firstLine = requestWholeText.substring(0, i - 1).split(" ");
+                break;
+            }
+        }
+        if (firstLine == null) {
+            log.warn("Invalid HTTP Request, has no first line");
+            throw new Exception("Invalid HTTP Request");
+        }
+
+        this.method = HttpMethod.valueOf(firstLine[0]);
+        this.protocol = firstLine[2];
+
+        i = parseHeaders(i, requestWholeText);
 
         switch (method) {
             case GET:
-                uri = parseUri(requestHeader[1]);
-                parseUriAndParametersForGET(uri.length(), requestHeader[1]);
+                uri = firstLine[1].split("\\?")[0];
+                parseParameters(uri.length(), firstLine[1]);
                 break;
             case POST:
-                uri = requestHeader[1];
+                uri = firstLine[1];
+                parseParameters(i - 1, requestWholeText);
                 break;
             default:
                 uri = "";
         }
     }
 
-    private static String parseUri(String wholeUriText) {
+    private int parseHeaders(int baseOffset, String requestWholeText) {
         int i;
-        for (i = 0; i < wholeUriText.length(); i++) {
-            if (wholeUriText.charAt(i) == '?') {
-                break;
+        for (i = baseOffset + 1; i < requestWholeText.length(); i++) {
+            if (requestWholeText.charAt(i - 1) == '\r' && requestWholeText.charAt(i) == '\n') {
+                if (i - baseOffset < 3) {
+                    break;
+                }
+                String[] currentHeader = requestWholeText.substring(baseOffset + 1, i - 1).split(": ");
+                headers.put(currentHeader[0], currentHeader[1]);
+                baseOffset = i;
             }
         }
-        return wholeUriText.substring(0, i);
+        return i + 1;
     }
 
-    private void parseUriAndParametersForGET(int offset, String wholeUri) {
+    private void parseParameters(int offset, String wholeUri) {
+        if (offset + 1 >= wholeUri.length()) {
+            return;
+        }
         String parametersString = wholeUri.substring(offset + 1);
         parseParameters(parametersString);
     }
